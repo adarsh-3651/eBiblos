ba311706cb854bc5af316ca7d1cd5a483fb1cafa
Adarsh <np03cs4s240013@heraldcollege.edu.np>
2025-03-20 23:02:25 +0545

    Checking tailwind integration

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>We are building eBiblos</h1>
-    </>
+    <div className='bg-gray-600'>
+    <h1 className='bg-white-300'>We are building eBiblos</h1>
+    <h2 className='bg-white-300'>This is a test</h2>
+    <h3 className='bg-white-300'>This is a test</h3>
+    <button
+      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+      onClick={() => {
+        setCount(count + 1)
+        console.log(count)}
+        }>Click me</button>
+    </div>
   )
 }
 
