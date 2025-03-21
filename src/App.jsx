2b9d695c6d25d79b8a1cc47abb92b2d140cff85a
Adarsh <np03cs4s240013@heraldcollege.edu.np>
2025-03-21 22:09:46 +0545

    Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { useState } from 'react'
-
+import Login from './Login'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-gray-600'>
-    <h1 className='bg-white-300'>We are building eBiblos</h1>
-    <h2 className='bg-white-300'>This is a test</h2>
-    <h3 className='bg-white-300'>This is a test</h3>
-    <button
-      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-      onClick={() => {
-        setCount(count + 1)
-        console.log(count)}
-        }>Click me</button>
-    </div>
+    <>
+      <Login />
+    </>
   )
 }
 
