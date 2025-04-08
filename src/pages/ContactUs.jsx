b270d61d988d7acf34eab3e6304842028e374796
Rabindra <rabin9811768489@gmail.com>
2025-04-08 11:15:50 +0545

    Created Contact Us to solve issues of user

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+      <div className="page">
+        <h2>Contact Us</h2>
+        <p>Reach out to us via email at support@example.com</p>
+      </div>
+    );
+  };
+  
+  export default Contact;
