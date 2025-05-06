838c6f0368835f8fa9abbd1da0148ed1b1438bf9
Farhan <np03cs4a230270@heraldcollege.edu.np>
2025-05-06 11:46:36 +0545

    "feat: create basic Features component with placeholder content"

--- src/pages/Footer/Features.jsx
+++ src/pages/Footer/Features.jsx
@@ -1,9 +1,13 @@
+// Importing the React library to use JSX
 import React from 'react'
 
+// Defining a functional component named Features
 function Features() {
   return (
+    // JSX that returns a simple <div> with the text "Features"
     <div>Features</div>
   )
 }
 
+// Exporting the Features component for use in other parts of the app
 export default Features
