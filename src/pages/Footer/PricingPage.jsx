7361e9e823a2e7d7691052283caa07f0400c5772
Farhan <np03cs4a230270@heraldcollege.edu.np>
2025-05-06 11:50:54 +0545

    create basic PricingPage component with placeholder

--- src/pages/Footer/PricingPage.jsx
+++ src/pages/Footer/PricingPage.jsx
@@ -1,9 +1,13 @@
+// Importing React to use JSX syntax and create a component
 import React from 'react'
 
+// Defining a functional component named PricingPage
 function PricingPage() {
   return (
+    // JSX returns a simple div with the text "PricingPage"
     <div>PricingPage</div>
   )
 }
 
+// Exporting the component so it can be used in other parts of the application
 export default PricingPage
