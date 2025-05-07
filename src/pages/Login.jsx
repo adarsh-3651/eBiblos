11b0b87aaedd3b593c34f776ec1543c07a0fb085
Farhan <np03cs4a230270@heraldcollege.edu.np>
2025-05-07 09:59:52 +0545

    create login page and render Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Login as loginComponent } from '../components'
+import React from 'react';
+// Import the Login component from components directory
+import { Login as loginComponent } from '../components';
+
 function Login() {
   return (
+    // Apply vertical padding to the container
     <div className='py-8'>
+        {/* Render the Login component */}
         <loginComponent />
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
